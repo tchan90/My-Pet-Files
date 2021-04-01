49a0cfbc3446ea953e68d1f62bdd6f3fe0d83447
Tammy Chan <tammy.chan@genero.com>
2021-04-01 12:02:27 +1100

    home page edits

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  PlayfairDisplay_700Bold,
+} from '@expo-google-fonts/playfair-display';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    PlayfairDisplay_700Bold,
+  });
+  // if (!fontsLoaded) {
+  //   return <AppLoading />;
+  // }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
+      <Text style={styles.heading}>My Pet Files</Text>
+      <Image
+        style={styles.homeImage}
+        source={require('./assets/PetFilesImage.png')}
+      />
+      <Button
+        onPress={() => {}}
+        title="Register"
+        color="#6223c7"
+        accessibilityLabel="Register"
+      />
     </View>
   );
 }
@@ -18,4 +39,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    fontSize: 30,
+    fontFamily: 'PlayfairDisplay_700Bold',
+  },
+  homeImage: {
+    width: '90%',
+    height: '50%',
+    resizeMode: 'contain',
+  },
+  buttonStyle: {},
 });
